78e930b2726b16a2196940988e705098df4f753b
toraud96 <56538394+toraud96@users.noreply.github.com>
2020-03-21 20:30:27 -0500

    update

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,28 +1,22 @@
+//from data.js (used day 3 activity 3)
+var data=data;
+console.log(data);
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 //Bringing the information to the html to be put in a table
-data.forEach(function(eyeWitness) {
-    console.log(eyeWitness);
-    var row = tbody.append("tr");
-    Object.entries(eyeWitness).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the eye witness report object
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-    
-//break might happen after this comment
-
-// Assign the data from `data.js` to a descriptive variable
-// var tbody = data;
-
-// Select the button
-var button = d3.select("#filter-btn");
+data.forEach((eyeWitness) => {
+var row = tbody.append("tr");
+Object.entries(eyeWitness).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });   
+});
+//button (used day3 activity 9)
+var button=d3.select("#filter-btn");
 
 button.on("click", function() {
+  tbody.html("");  
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
@@ -31,8 +25,19 @@ button.on("click", function() {
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
-  console.log(people);
 
-  var filteredData = people.filter(person => person.datetime === inputValue);
+  var filteredData = data.filter(witness => witness.datetime === inputValue);
 
   console.log(filteredData);
+
+  //Revealing data table again with the filtered options
+  //I wonder if there's a simpler way to get the filtered table than writing the same code again? 
+
+  filteredData.forEach((filteredEyeWitness) => {
+  var row = tbody.append("tr");
+  Object.entries(filteredEyeWitness).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
+});
