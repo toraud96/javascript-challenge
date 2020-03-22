9caa1e01483b692ef067d74122ec483e51478a3d
toraud96 <56538394+toraud96@users.noreply.github.com>
2020-03-21 19:03:23 -0500

    update

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,38 @@
-// from data.js
-var tableData = data;
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+//Bringing the information to the html to be put in a table
+data.forEach(function(eyeWitness) {
+    console.log(eyeWitness);
+    var row = tbody.append("tr");
+    Object.entries(eyeWitness).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the eye witness report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+    
+//break might happen after this comment
+
+// Assign the data from `data.js` to a descriptive variable
+// var tbody = data;
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(people);
+
+  var filteredData = people.filter(person => person.datetime === inputValue);
+
+  console.log(filteredData);
